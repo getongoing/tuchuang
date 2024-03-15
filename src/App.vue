bdb31ab1833e074c0f5b0aeaded1430ae8416bc7
一男 <minzufeng@vip.qq.com>
2024-03-15 16:05:07 +0800

    修复No known conditions语言包报错

--- src/App.vue
+++ src/App.vue
@@ -54,7 +54,7 @@
 import { faCog, faUpload } from '@fortawesome/free-solid-svg-icons'
 import { useRouter } from 'vue-router'
 import { ElScrollbar, ElConfigProvider } from 'element-plus'
-import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import zhCn from 'element-plus/dist/locale/zh-cn'
 
 const repoLink = 'https://roim.app'
 const repoName = 'roim-picx'
